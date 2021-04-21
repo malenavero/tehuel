27f5f4be7a9a7a9550a15b0024316cabe344903d
Malena Vero <58344888+malenavero@users.noreply.github.com>
2021-04-21 01:53:10 -0300

    corregido timezone-utc-00

--- js/index.js
+++ js/index.js
@@ -1,14 +1,13 @@
-
 function diasSinTehuel(){
-    const initialDate = new Date("2021-03-11")
-    let actualDate = new Date()
+    /* lo construyo con "," en vez de "-" para setearlo en utc */
+    const initialDate = new Date("2021,03,11")
+    /* construyo la fecha actual con anio + mes + dia para poder tenerlo en time 00 de utc */
+    let actualDate = new Date(new Date().getUTCFullYear(), new Date().getUTCMonth() , new Date().getUTCDate())
+
     let diff = actualDate.getTime() - initialDate.getTime();
     let daysPassed = Math.round(diff / (1000 * 60 * 60 * 24));
     document.body.innerHTML = `<article><h1 class="parpadea">${daysPassed} DÍAS SIN TEHUEL<h1></article>`;
     return daysPassed
 }
-
-/* como hago para que no me tome la hr y me tome a partir de las 00hrs */
-console.log(diasSinTehuel())
 
 
