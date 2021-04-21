1d96a4859a8ce61d7e9ad1a191a8f2cfd8b4614a
Malena Vero <58344888+malenavero@users.noreply.github.com>
2021-04-20 21:27:56 -0300

    primer commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+function diasSinTehuel(){
+    const initialDate = new Date("2021-03-11")
+    let actualDate = new Date()
+    let diff = actualDate.getTime() - initialDate.getTime();
+    let daysPassed = Math.round(diff / (1000 * 60 * 60 * 24))-1;
+    document.body.innerHTML = `<article><h1>${daysPassed} DÍAS SIN TEHUEL<h1></article>`;
+    return daysPassed
+}
+
+/* como hago para que no me tome la hr y me tome a partir de las 00hrs */
+console.log(diasSinTehuel())
+
+
