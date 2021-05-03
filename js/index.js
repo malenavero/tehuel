f56b6f032b01b293435d5bfa177e40a094ebb5eb
Malena Vero <58344888+malenavero@users.noreply.github.com>
2021-05-03 18:40:29 -0300

    agregue el event listener y modifique para usar inner text en vez de inner html

--- js/index.js
+++ js/index.js
@@ -1,13 +1,15 @@
-function diasSinTehuel(){
-    /* lo construyo con "," en vez de "-" para setearlo en utc */
-    /* const initialDate = new Date(2021,02,11) */
-    /* construyo la fecha actual con anio + mes + dia para poder tenerlo en time 00 de utc */
-    /*     let actualDate= new Date(new Date().getFullYear(), new Date().getMonth() , new Date().getDate())
-    let diff = actualDate.getTime() - initialDate.getTime();*/
-    let daysPassed = Math.floor( ( ( new Date() ) - ( new Date("2021-03-11T00:00:00.000-03:00") ) ) / ( 1000 * 60 * 60 * 24 ) )
-    document.body.innerHTML = `<article><h1 class="parpadea">${daysPassed} DÍAS SIN TEHUEL</h1></article>`; 
+window.addEventListener('load', diasSinTehuel);
 
-    return daysPassed
+function diasSinTehuel(){
+    let daysPassed = Math.floor( ( ( new Date() ) - ( new Date("2021-03-11T00:00:00.000-03:00") ) ) / ( 1000 * 60 * 60 * 24 ) )
+
+    let days = document.querySelector('#dias');
+
+    days.innerText = daysPassed
+
 }
+
+
+
 
 
